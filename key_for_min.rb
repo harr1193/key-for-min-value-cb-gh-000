a18d5a16bbcb7a784aae6df222c8537f4bd5555b
Kyle Harris <k_harris95@live.com>
2018-01-23 21:01:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,6 @@
 def key_for_min_value(name_hash)
   min_key = ""
   min_value = ""
-  name_hash.collect { |key, value| min_key.empty? || value < name_hash[min_key] ? min_key = key : min_key = nil }
+  name_hash.collect { |key, value| min_key.empty? || value < name_hash[min_key] ? min_key = key : nil }
   min_key
 end
