f3aa24e63fd4ccb76a6cfd3bcd7a91d89da69a1b
Kyle Harris <k_harris95@live.com>
2018-01-23 20:58:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,6 @@
 def key_for_min_value(name_hash)
   min_key = ""
   min_value = ""
-  name_hash.collect { |key, value| min_key = key; if min_key.empty? || name_hash[key] < min_key
+  name_hash.collect { |key, value| min_key = key if min_key.empty? || name_hash[key] < min_key
    }
 end
